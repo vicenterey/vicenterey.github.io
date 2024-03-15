f00998ea0746a031beb7e3f598801f84ad34dd57
vicenterey <virey@miuandes.cl>
2024-03-15 11:11:06 -0300

    firstcommit

---
+++ main.js
@@ -0,0 +1,5 @@
+function TextChanger() {
+    var input = document.getElementById('textinput');
+    var ChangedText = document.getElementById('changedtext');
+    ChangedText.innerHTML = input.value;
+}
